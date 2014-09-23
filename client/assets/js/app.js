66aca8c0cf1cfc6931740d5214d9904ddbbcd2f3
Antonin Januska <antonin@antjanus.com>
2014-09-23 07:33:37 -0500

    UI Router prototype for testing

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -1,1 +1,15 @@
-var app = angular.module('application', []);
+var app = angular.module('application', ['ui.router'])
+    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlProvider) {
+
+  $urlProvider.otherwise('/');
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'templates/home.html'
+    })
+    .state('page', {
+      url: '/page',
+      templateUrl: 'templates/page.html'
+    })
+}])
