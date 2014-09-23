207a12a08af9693a877fcb6f03acec2f0f071b79
Antonin Januska <antonin@antjanus.com>
2014-09-23 11:04:24 -0500

    Front matter implementation and variable gathering

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
-var gulp = require('gulp'),
-    rimraf = require('rimraf'),
-    server = require('gulp-develop-server');
+var gulp        = require('gulp'),
+    rimraf      = require('rimraf'),
+    server      = require('gulp-develop-server'),
+    frontMatter = require('gulp-front-matter'),
+    path        = require('path');
 
-gulp.task('server:start', ['copy'], function() {
+gulp.task('server:start', ['front-matter'], function() {
   server.listen( { path: 'app.js' });
 });
 
@@ -15,6 +17,30 @@ gulp.task('copy', ['clean'], function() {
     .pipe(gulp.dest('build'));
 });
 
-gulp.task('default', ['copy', 'server:start'], function() {
+gulp.task('front-matter', ['copy'], function() {
+  var root = [];
+
+  gulp.src('./client/templates/*.html')
+    .pipe(frontMatter({
+      property: 'meta',
+      remove: true
+    }))
+    .on('data', function(data) {
+      page = data.meta;
+
+      //path normalizing
+      relativePath = path.relative(__dirname + path.sep + 'client', data.path);
+      page.path = relativePath.split(path.sep).join('/');
+
+      root.push(page);
+    }).pipe(gulp.dest('build/templates'))
+    .on('end', function(data) {
+      console.log(root);
+      console.log('made it!');
+    })
+  ;
+});
+
+gulp.task('default', ['copy', 'front-matter', 'server:start'], function() {
   gulp.watch('client/**/*.*', ['copy', server.restart]);
 });
