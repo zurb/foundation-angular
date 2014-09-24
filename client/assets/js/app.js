685eaf5b73d06a56bec6580a8c9bf4901c6cdb99
Antonin Januska <antonin@antjanus.com>
2014-09-24 08:23:09 -0500

    Default controller and parameter page demo

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -1,14 +1,21 @@
 var app = angular.module('application', ['ui.router'])
     .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlProvider) {
 
-  $urlProvider.otherwise('/');
+    $urlProvider.otherwise('/');
 
     angular.forEach(dynamicRoutes, function(page) {
       var state = {};
       state.url = page.url;
       state.templateUrl = page.path;
-      state.parent = page.parent ? page.parent : '';
-      state.controller = page.controller ? page.controller : '';
+      state.parent = page.parent || '';
+      state.controller = page.disableController || function($scope, $stateParams) {
+        var params = [];
+        angular.forEach($stateParams, function(value, key) {
+          params[key] = value;
+        });
+
+        $scope.params = params;
+      }
 
       $stateProvider.state(page.name, state);
     });
