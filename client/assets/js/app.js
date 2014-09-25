7deb4b7433665b6f3fe26ffff7aeba42a0596e95
Antonin Januska <antonin@antjanus.com>
2014-09-25 07:40:14 -0500

    Refactor for cleaner code

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -1,69 +1,65 @@
+
 var app = angular.module('application', ['ui.router'])
     .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlProvider) {
 
     $urlProvider.otherwise('/');
+
     var complexViews = {};
 
     angular.forEach(dynamicRoutes, function(page) {
-      //I am not proud of this code, I'll refactor when it works
       if (page.hasAbstract == true) {
         if (!angular.isDefined(complexViews[page.parent])) {
-          complexViews[page.parent]['children'] = [];
+          complexViews[page.parent] = { children: [] };
         }
+
         complexViews[page.parent]['children'].push(page);
       } else if (page.abstract == true) {
         if(!angular.isDefined(complexViews[page.name])) {
-          complexViews[page.name] = {};
-          complexViews[page.name]['children'] = [];
+          complexViews[page.name] = { children: [] };
         }
 
-        angular.forEach(page, function(value, key) {
-          complexViews[page.name][key] = value;
-        });
+        angular.extend(complexViews[page.name], page);
       } else {
-        var state = {};
-        state.url = page.url;
-        state.templateUrl = page.path;
-        state.parent = page.parent || '';
-        state.controller = page.disableController ? '' : function($scope, $stateParams) {
-          var params = [];
-          angular.forEach($stateParams, function(value, key) {
-            params[key] = value;
-          });
-
-          $scope.params = params;
-          $scope.vars = page;
-        }
+        var state = {
+          url: page.url,
+          templateUrl: page.path,
+          parent: page.parent || '',
+          controller: page.controller || 'DefaultController',
+          resolve: { vars: function() { return page } },
+        };
 
         $stateProvider.state(page.name, state);
       }
     });
 
     angular.forEach(complexViews, function(page) {
-        var state = {};
-        state.url = page.url;
-        state.parent = page.parent || '';
-        state.controller = page.disableController ? '' : function($scope, $stateParams) {
-          var params = [];
-          angular.forEach($stateParams, function(value, key) {
-            params[key] = value;
-          });
-
-          $scope.params = params;
-          $scope.vars = page;
-        }
-
-        state.views = {};
-        state.views[''] = {};
-        state.views[''].templateUrl = page.path;
+        var state = {
+          url: page.url,
+          parent: page.parent || '',
+          controller: page.controller || 'DefaultController',
+          resolve: { vars: function() { return page; } },
+          views: { '': { templateUrl: page.path } }
+        };
 
         angular.forEach(page.children, function(sub) {
-          var title = sub.name + '@' + page.name;
-          state.views[title] = {};
-          state.views[title].templateUrl = sub.path;
+          state.views[sub.name + '@' + page.name] = {
+            templateUrl: sub.path,
+            resolve: { vars: function() { return sub; } }
+            };
         });
 
         $stateProvider.state(page.name, state);
     });
 }]);
 
+angular.module('application')
+  .controller('DefaultController', ['$scope', '$stateParams', 'vars', function($scope, $stateParams, vars) {
+          var params = [];
+          angular.forEach($stateParams, function(value, key) {
+            params[key] = value;
+          });
+
+          $scope.params = params;
+          $scope.vars = vars;
+        }
+    ]);
