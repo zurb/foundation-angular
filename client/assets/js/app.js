81dfb17cf36e61cc911d0303af7bc83324c23a9d
Antonin Januska <antonin@antjanus.com>
2014-09-26 12:43:31 -0500

    Slugify

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -56,17 +56,26 @@ var app = angular.module('application', ['ui.router', 'ngAnimate'])
 
 angular.module('application')
   .controller('DefaultController', ['$scope', '$stateParams', '$state', function($scope, $stateParams, $state) {
-          var params = [];
-          angular.forEach($stateParams, function(value, key) {
-            params[key] = value;
-          });
+    var params = [];
+    angular.forEach($stateParams, function(value, key) {
+      params[key] = value;
+    });
 
-          $scope.params = params;
-          if($state.current.views) {
-            $scope.vars = $state.current.data.vars;
-            $scope.composed = $state.current.data.vars.children;
-          } else {
-            $scope.vars = $state.current.data.vars;
-          }
-        }
-    ]);
+    $scope.params = params;
+    $scope.current = $state.current.name;
+
+    if($state.current.views) {
+      $scope.vars = $state.current.data.vars;
+      $scope.composed = $state.current.data.vars.children;
+    } else {
+      $scope.vars = $state.current.data.vars;
+    }
+  }
+]);
+
+angular.module('application')
+  .filter('slugify', function() {
+      return function(input) {
+        return input.replace(/\./, '-').toLowerCase();
+      };
+});
